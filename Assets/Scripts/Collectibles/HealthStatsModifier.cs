5e1ee1393a449eb3b374a73979b665f25bf0bec7
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-25 19:53:10 +0300

    Update HealthStatsModifier.cs

--- Assets/Scripts/Collectibles/HealthStatsModifier.cs
+++ Assets/Scripts/Collectibles/HealthStatsModifier.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class HealthStatsModifier : MonoBehaviour , IStatsModifier
 {
     [SerializeField]private int healthToAdd = 25;
+    
     public void addToCharacter(GameObject player)
     {
         player.GetComponent<Health>()?.addHealth(healthToAdd);
