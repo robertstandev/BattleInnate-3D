742ffb49a8bbabdf4cca8b2c75dc83460052179a
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-25 10:32:23 +0300

    Add files via upload

---
+++ Assets/Scripts/Humanoid/Player/Scripts/Other/PlayerManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerManager : MonoBehaviour
+{   
+    [SerializeField]private Movement movementComponent;
+    private void Awake()
+    {
+        movementComponent = GetComponent<Movement>();
+    }
+
+  private void FixedUpdate() {
+      
+  }
+
+}
