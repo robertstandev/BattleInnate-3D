9dde3005a12b0a3b0aa96572516b587bf3f57154
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-25 19:43:44 +0300

    Add files via upload

---
+++ Assets/Scripts/Collectibles/StaminaStatsModifier.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaminaStatsModifier : MonoBehaviour , IStatsModifier
+{
+    public void addToCharacter(GameObject player)
+    {
+        player.GetComponent<Stamina>()?.addStamina(50);
+    }
+}
