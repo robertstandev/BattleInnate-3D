3bb2adeec00588f92cd92a65fe836bf58c934e87
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-25 19:42:12 +0300

    Add files via upload

---
+++ Assets/Scripts/General/Health.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int currentHealth = 100;
+
+    public void addHealth(int value)
+    {
+        currentHealth = (currentHealth + value) <= 100 ? currentHealth + value : 100;
+    }
+}
---
+++ Assets/Scripts/General/Stamina.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stamina : MonoBehaviour
+{
+    private int currentStamina = 100;
+
+    public void addStamina(int value)
+    {
+        currentStamina = (currentStamina + value) <= 100 ? currentStamina + value : 100;
+    }
+}
