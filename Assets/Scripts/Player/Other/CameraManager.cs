ff838be13f084b77e2ad757bfbbd56db5175a1aa
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-25 18:25:31 +0300

    Update CameraManager.cs

--- Assets/Scripts/Player/Other/CameraManager.cs
+++ Assets/Scripts/Player/Other/CameraManager.cs
@@ -4,4 +4,16 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    [SerializeField]private GameObject player;
+    private Vector3 cameraOffset;
+
+    private void Start()
+    {
+        cameraOffset = transform.position - player.transform.position;
+    }
+
+    private void LateUpdate() 
+    {
+        transform.position = player.transform.position + cameraOffset;
+    }
 }
