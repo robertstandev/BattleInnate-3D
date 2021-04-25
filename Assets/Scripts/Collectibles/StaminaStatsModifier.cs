6061013cd2d03a17e820b2685ae2c0afcaefa208
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-25 19:52:54 +0300

    Update StaminaStatsModifier.cs

--- Assets/Scripts/Collectibles/StaminaStatsModifier.cs
+++ Assets/Scripts/Collectibles/StaminaStatsModifier.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class StaminaStatsModifier : MonoBehaviour , IStatsModifier
 {
     [SerializeField]private int staminaToAdd = 50;
+    
     public void addToCharacter(GameObject player)
     {
         player.GetComponent<Stamina>()?.addStamina(staminaToAdd);
