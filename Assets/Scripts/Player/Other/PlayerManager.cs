692ba42556ef48139471553ebad057cd595afeab
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-26 19:40:18 +0300

    Update PlayerManager.cs

--- Assets/Scripts/Player/Other/PlayerManager.cs
+++ Assets/Scripts/Player/Other/PlayerManager.cs
@@ -8,14 +8,12 @@ public class PlayerManager : MonoBehaviour
     private Movement movementComponent;
     private Jump jumpComponent;
     private Stamina staminaComponent;
-    private Health healthComponent;
 
     private void Awake()
     {
         movementComponent = GetComponent<Movement>();
         jumpComponent = GetComponent<Jump>();
         staminaComponent = GetComponent<Stamina>();
-        healthComponent = GetComponent<Health>();
     }
 
     private void OnMove(InputValue movementValue) { movementComponent.moveCharacter(movementValue.Get<Vector2>()); }
