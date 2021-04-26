6a7048fbb406feb2913c9da76db463ca81cecfac
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-26 09:25:06 +0300

    Update Movement.cs

--- Assets/Scripts/General/Movement.cs
+++ Assets/Scripts/General/Movement.cs
@@ -1,24 +1,21 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Movement : MonoBehaviour
 {
     [SerializeField]private float movementSpeed = 10f;
-    private Rigidbody rb;
     private Vector3 movementVector3 = Vector3.zero;
+    private Rigidbody rb;
 
-    private void Awake() {
-        rb = GetComponent<Rigidbody>();
-    }
+    private void Awake() { rb = GetComponent<Rigidbody>(); }
+
+    private void FixedUpdate() { rb.AddForce(movementVector3 * movementSpeed); }
 
     public void moveCharacter(Vector2 movementVector2)
     {
         movementVector3.x = movementVector2.x;
         movementVector3.z = movementVector2.y;
-    }
-
-    private void FixedUpdate(){
-        rb.AddForce(movementVector3 * movementSpeed);
     }
 }
