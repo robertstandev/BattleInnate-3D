a95503b89af3f1dc19c928a27142050bfce4a6ef
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-26 09:25:57 +0300

    Update Jump.cs

--- Assets/Scripts/General/Jump.cs
+++ Assets/Scripts/General/Jump.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Jump : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]private float jumpPower = 10f;
+    [SerializeField]private int maxJumps = 2;
+    private int jumpCounter = 0;
+    private Vector3 jumpVector3 = new Vector3(0f, 100f, 0f);
+    private Rigidbody rb;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake() { rb = GetComponent<Rigidbody>(); }
+
+    public void jump()
     {
-        
+        if(jumpCounter < maxJumps){
+            jumpCounter += 1;
+            rb.AddForce(jumpVector3 * jumpPower);
+        }
     }
 }
