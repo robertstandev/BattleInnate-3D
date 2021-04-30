a15767b69c7d8726e8561b31a37830c1ce76c34f
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-30 21:15:30 +0300

    Add files via upload

---
+++ Assets/Scripts/Modes/Arena/ArenaMapModifier.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaMapModifier : MonoBehaviour
+{
+    [SerializeField]private Transform ground;
+    [SerializeField]private Vector3 shrinkGroundPerTick = new Vector3(0.1f, 0.1f, 0f);
+    private Timer timerComponent;
+    private int mapModifierTimerInstance;
+
+    private void Awake() { timerComponent = GetComponent<Timer>(); }
+
+    private void OnEnable()
+    { 
+        mapModifierTimerInstance = timerComponent.createTimerInstanceAndGetIndex(0.5f, scaleMap);
+        timerComponent.startTimer(mapModifierTimerInstance);
+    }
+
+    private void OnDisable() { timerComponent.stopTimer(mapModifierTimerInstance); }
+
+    private void scaleMap()
+    {
+        if(ground.localScale.x < 10f || ground.localScale.y < 10f)
+        {
+            timerComponent.stopTimer(mapModifierTimerInstance);
+        }
+        else
+        {
+            ground.localScale -= shrinkGroundPerTick;
+        } 
+    }
+}
