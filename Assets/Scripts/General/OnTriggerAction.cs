3d4fddb2905aace014f069ebcdc807cd3943f63a
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-05-01 21:30:09 +0300

    Add files via upload

---
+++ Assets/Scripts/General/OnTriggerAction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerAction : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<IOnTriggerAction>() != null)
+        {
+            other.gameObject.GetComponent<IOnTriggerAction>().doAction();
+        }
+    }  
+}
