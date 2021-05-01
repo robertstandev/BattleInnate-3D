3f7197e7812a70202173f1d39183071abe187da4
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-05-01 20:03:20 +0300

    Add files via upload

---
+++ Assets/Scripts/Modes/MainMenu/ModeSelect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ModeSelect : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField]private int sceneNumber = 0;
+    private ScenesManager scenesManager;
+
+    private void Awake()
+    {
+        scenesManager = transform.parent.GetComponent<ScenesManager>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        scenesManager.loadScene(sceneNumber);
+    }
+}
