84538710a54e32bf03b719ed10a6c4d6dd600377
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-05-01 21:10:04 +0300

    Add files via upload

---
+++ Assets/Scripts/Player/Other/RespawnPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnPlayer : MonoBehaviour , IRespawn
+{
+    [SerializeField]private Vector3 position;
+    [SerializeField]private Quaternion rotation;
+    private Rigidbody rb;
+    private PlayerControllerMove controllerMoveComponent;
+    private PlayerControllerJump controllerJumpComponent;
+
+    private void Awake()
+    { 
+        rb = GetComponent<Rigidbody>();
+        controllerMoveComponent = GetComponent<PlayerControllerMove>();
+        controllerJumpComponent = GetComponent<PlayerControllerJump>();
+    }
+
+    public void respawn()
+    {
+        resetComponents();
+        changePositionAndRotation();
+    }
+
+    private void changePositionAndRotation() { transform.SetPositionAndRotation(position , rotation); }
+
+    private void resetComponents()
+    {
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+
+        if(controllerMoveComponent != null)
+        {
+            controllerMoveComponent.enabled = true;
+        }
+        
+        if(controllerJumpComponent != null)
+        {
+            controllerJumpComponent.enabled = true;
+        }
+
+    }
+}
