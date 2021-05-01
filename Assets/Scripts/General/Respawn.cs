d6a0d5429bf1251ddfb2923c40fe98c769eaa455
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-05-01 21:11:23 +0300

    Add files via upload

---
+++ Assets/Scripts/General/Respawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<IRespawn>() != null)
+        {
+            other.gameObject.GetComponent<IRespawn>().respawn();
+        }
+    }  
+}
