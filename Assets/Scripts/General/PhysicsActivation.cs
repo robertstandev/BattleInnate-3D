1d3660d75e288ef810ffb77fed855758deacb80f
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-05-01 11:20:23 +0300

    Add files via upload

---
+++ Assets/Scripts/General/PhysicsActivation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsActivation : MonoBehaviour
+{
+    [SerializeField]private float activateGravityAfterSec = 3f;
+    private Rigidbody rbComponent;
+    private Timer timerComponent;
+    private int activateGravityTimerInstance;
+
+    private void Awake()
+    { 
+        timerComponent = GetComponent<Timer>();
+        rbComponent = GetComponent<Rigidbody>();
+    }
+    
+    private void OnEnable() { activateGravityTimerInstance = timerComponent.createTimerInstanceAndGetIndex(activateGravityAfterSec, activateGravity); }
+
+    private void OnDisable() { timerComponent.stopTimer(activateGravityTimerInstance); }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            timerComponent.startTimer(activateGravityTimerInstance);
+        }
+    }
+
+    private void activateGravity(){
+        rbComponent.isKinematic = false;
+        rbComponent.useGravity = true;
+    }
+}
