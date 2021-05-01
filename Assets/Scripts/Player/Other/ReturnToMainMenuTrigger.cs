8e45a1db56d08133132d636680c8f8471aefcea7
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-05-01 23:11:28 +0300

    Add files via upload

---
+++ Assets/Scripts/Player/Other/ReturnToMainMenuTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMainMenuTrigger : MonoBehaviour , IOnTriggerAction
+{
+    private Timer timerComponent;
+    private int returnToMainMenuTimerInstance;
+
+    private void Awake() { timerComponent = GetComponent<Timer>(); }
+    
+    public void doAction()
+    { 
+        returnToMainMenuTimerInstance = timerComponent.createTimerInstanceAndGetIndex(false, 3f, returnToMainMenu);
+        timerComponent.startTimer(returnToMainMenuTimerInstance);
+        
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        GetComponent<Renderer>().enabled = false;
+    }
+
+    private void returnToMainMenu() { SceneManager.LoadSceneAsync(0, LoadSceneMode.Single); }
+}
