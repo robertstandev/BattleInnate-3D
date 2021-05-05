aec6387ff02bc820ca2fba2f5e27daf6f2f3e4ff
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-05-05 11:04:53 +0300

    Add files via upload

--- Assets/Scripts/Modes/Maze/MazeFinishLine.cs
+++ Assets/Scripts/Modes/Maze/MazeFinishLine.cs
@@ -6,8 +6,12 @@ using UnityEngine.UI;
 public class MazeFinishLine : MonoBehaviour, IOnTriggerAction
 {
     [SerializeField]private Text textCanvas;
-    public void doAction()
+    public void activateTriggerEvent(GameObject triggeredObject)
     {
+        triggeredObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
         textCanvas.enabled = true;
+        Invoke("returnToMainMenu", 3f);
     }
+
+    private void returnToMainMenu() { transform.GetComponent<ScenesManager>().returnToMainMenu(); }
 }
