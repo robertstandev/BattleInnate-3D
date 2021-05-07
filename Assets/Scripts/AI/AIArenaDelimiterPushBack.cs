99d2eb86d2fd1de7734d0acca683fbef1f0caee8
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-05-07 11:42:34 +0300

    Add files via upload

---
+++ Assets/Scripts/AI/AIArenaDelimiterPushBack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIArenaDelimiterPushBack : MonoBehaviour, IOnTriggerAction
+{
+    private Rigidbody rb;
+
+    public void activateTriggerEvent(GameObject triggeredObject)
+    {
+        if (!triggeredObject.CompareTag("Enemy"))
+        {
+            return;
+        }
+        else
+        {
+            rb = triggeredObject.GetComponent<Rigidbody>();
+            rb.angularVelocity = Vector3.zero;
+            rb.velocity = Vector3.zero;
+        }
+    }
+}
